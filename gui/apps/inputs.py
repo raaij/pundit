4034f2e6ebcd40acb08d7d242d8d8e998dc5a0f1
AlexTsiamas <97591187+AlexTsiamas@users.noreply.github.com>
2022-01-16 13:08:11 +0200

    Update inputs.py

--- gui/apps/inputs.py
+++ gui/apps/inputs.py
@@ -19,17 +19,17 @@ layout = html.Div(
                         dbc.Col(
                             [
                                 dbc.Row("Headers"),
-                                dbc.Row("Header 1"),
-                                dbc.Row("Header 2"),
+                                dbc.Row(dbc.Input(id="input1".format("text"), placeholder="Header 1".format("text"))),
+                                dbc.Row(dbc.Input(id="input2".format("text"), placeholder="Header 2".format("text"))),
                                 dbc.Row("..."),
                             ]
                         ),
                         dbc.Col(
                             [
-                                dbc.Row("Descriptions"),
-                                dbc.Row("Description 1"),
-                                dbc.Row("Description 2"),
-                                dbc.Row("..."),
+                                dbc.Row("Images"),
+                                dbc.Row(dbc.Input(id="input3".format("text"), placeholder="Image 1".format("text"))),
+                                dbc.Row(dbc.Input(id="input4".format("text"), placeholder="Image 2".format("text"))),
+                                dbc.Row("...")
                             ]
                         ),
                     ]
@@ -38,22 +38,36 @@ layout = html.Div(
                     [
                         dbc.Col(
                             [
-                                dbc.Row("Images"),
-                                dbc.Row("Image 1"),
-                                dbc.Row("Image 2"),
-                                dbc.Row("..."),
+                                dbc.Row("Descriptions"),
+                                dbc.Row(dbc.Input(id="input5".format("text"), placeholder="Description 1".format("text"))),
+                                dbc.Row(dbc.Input(id="input6".format("text"), placeholder="Description 2".format("text"))),
+                                dbc.Row("...")
                             ]
                         )
                     ]
                 ),
                 dbc.Row(
                     [
-                        dbc.Col([dbc.Row("Accuracy"), dbc.Row("Value")]),
-                        dbc.Col([dbc.Row("Confidence"), dbc.Row("Value")]),
-                        dbc.Col([dbc.Button("Run Experiment")]),
+                        dbc.Col(
+                            [
+                                dbc.Row("Accuracy"),
+                                dbc.Row(dbc.Input(id="input7".format("number"), placeholder="95% by default".format("text")))
+                            ]
+                        ),
+                        dbc.Col(
+                            [
+                                dbc.Row("Confidence"),
+                                dbc.Row(dbc.Input(id="input8".format("number"), placeholder="Enter Value".format("text")))
+                            ]
+                        ),
+                        dbc.Col([dbc.Button("Run Experiment", n_clicks=0)]),
                     ]
                 ),
             ],
         ),
     ],
 )
+
+def Json_saved(input1, input2, input3, input4, input5, input6, n_clicks):
+    if n_clicks:
+        return json.dumps(input1, input2, input3, input4, input5, input6)
