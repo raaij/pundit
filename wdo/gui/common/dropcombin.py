6f4ec2c66e56c1af573954cf328b195b8822ce2d
AlexTsiamas <97591187+AlexTsiamas@users.noreply.github.com>
2022-01-26 14:09:50 +0200

    Add files via upload

---
+++ wdo/gui/common/dropcombin.py
@@ -0,0 +1,26 @@
+import os
+import dash_bootstrap_components as dbc
+from dash import Input, Output, html
+from wdo.gui.app import app
+from dash import Dash, Input, Output, State, callback_context, dcc, html
+
+dropdown3 = html.Div([
+    dcc.Dropdown(
+        id='combin-dropdown',
+        options = [
+            {'label': 'Top 10 combinations', 'value': 'Top10'},
+            {'label': 'ALL', 'value': 'all'},
+        ],
+        placeholder="Choose Combinations for graph",
+        searchable=False
+    ),
+    html.Div(id='combin-container')
+])
+
+@app.callback(
+    Output('combin-container', 'children'),
+    Input('combin-dropdown', 'value')
+)
+
+def display_metric(value):
+    return 'You have selected "{}"'.format(value)
---
+++ wdo/gui/common/dropmetrics.py
@@ -0,0 +1,28 @@
+import os
+import dash_bootstrap_components as dbc
+from dash import Input, Output, html
+from wdo.gui.app import app
+from dash import Dash, Input, Output, State, callback_context, dcc, html
+
+
+dropdown2 = html.Div([
+    dcc.Dropdown(
+        id='metrics-dropdown',
+        options = [
+            {'label': 'impressions', 'value': 'impressions'},
+            {'label': 'clicks', 'value': 'clicks'},
+            {'label': 'ctr', 'value': 'click through rate (%)'}
+        ],
+        placeholder="Choose Metric",
+        searchable=False
+    ),
+    html.Div(id='output-container')
+])
+
+@app.callback(
+    Output('output-container', 'children'),
+    Input('metrics-dropdown', 'value')
+)
+
+def display_metric(value):
+    return 'You have selected "{}"'.format(value)
