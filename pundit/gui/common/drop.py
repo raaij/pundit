4cb954b5bc711b52909dccd22b0b40c973915b52
AlexTsiamas <97591187+AlexTsiamas@users.noreply.github.com>
2022-02-01 14:01:17 +0100

    Update drop.py

--- pundit/gui/common/drop.py
+++ pundit/gui/common/drop.py
@@ -1,7 +1,8 @@
 import os
-
+import dash
+from pundit.gui.app import app
 import dash_bootstrap_components as dbc
-
+from dash import Input, Output, html, dcc
 from pundit.constant import PATH_EXPERIMENTS
 
 files = os.listdir(PATH_EXPERIMENTS)
@@ -9,10 +10,26 @@ final_list = []
 for file in files:
     if file.endswith(".json"):
         final_list.append(file.replace(".json", ""))
-items = [dbc.DropdownMenuItem(i) for i in final_list]
+items = [dbc.DropdownMenuItem(i, id=i) for i in final_list]
 
-dropdown1 = dbc.DropdownMenu(
-    label="Choose Experiment",
-    direction="down",
-    children=items,
+
+dropdown1 = html.Div(
+    [
+        dbc.DropdownMenu(
+            children=items,
+            label="Choose Experiment"
+        ),
+        html.P(id="drop-experiment", className="mt-3"),
+    ]
 )
+@app.callback(
+    Output("drop-experiment", "children"),
+    [Input(i, "n_clicks") for i in final_list]
+)
+
+
+def display_experiment(*args):
+    ctx = dash.callback_context
+    if ctx.triggered:
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+        return "The Experiment is: " + button_id
