fd8efc031e8a241b4403fdc944e8d0f1eab2d873
lucien150 <55390506+lucien150@users.noreply.github.com>
2022-02-02 23:41:23 +0100

    Update drop.py

--- pundit/gui/common/drop.py
+++ pundit/gui/common/drop.py
@@ -1,6 +1,7 @@
 import os
 import dash
 from pundit.gui.app import app
+from pundit.gui.apps import inputs
 import dash_bootstrap_components as dbc
 from dash import Input, Output, html, dcc
 from pundit.constant import PATH_EXPERIMENTS
@@ -15,13 +16,22 @@ for file in files:
         final_list.append(file.replace(".json", ""))
 items = [dbc.DropdownMenuItem(i, id=i) for i in final_list]
 
-
 def fix_final_list():
+    fixed_list = []
+    file_list = os.listdir(PATH_EXPERIMENTS)
+    for file_1 in file_list:
+        if file_1.endswith(".json"):
+            fixed_list.append(file_1.replace(".json", ""))
+    return fixed_list
+
+def button_list(items):
+    final_list = []
     files = os.listdir(PATH_EXPERIMENTS)
-    for file1 in files:
-        if file1.endswith(".json"):
-            final_list.append(file1.replace(".json", ""))
-    items = [dbc.DropdownMenuItem(i, id=i) for i in final_list]
+    for file in files:
+        if file.endswith(".json"):
+            final_list.append(file.replace(".json", ""))
+    items = [dbc.DropdownMenuItem(i, id=i) for i in fix_final_list()]
+
     return items
 
 
@@ -29,7 +39,8 @@ dropdown1 = html.Div(
     [
         dbc.DropdownMenu(
             children=items,
-            label="Choose Experiment"
+            label="Choose Experiment",
+            id="labels"
         ),
         html.P(id="drop-experiment", className="mt-3"),
     ],
@@ -38,16 +49,24 @@ dropdown1 = html.Div(
 
 @app.callback(
     Output("drop-experiment", "children"),
-    [Input(i, "n_clicks") for i in final_list],
-    prevent_initial_callbacks=True
+    [Input(i, "n_clicks") for i in fix_final_list()],
 )
-
 
 def display_experiment(*args):
     ctx = dash.callback_context
     if ctx.triggered:
-        fix_final_list()
         button_id.append(ctx.triggered[0]["prop_id"].split(".")[0])
         return True
     else:
         return 'You have selected "None"'
+
+@app.callback(
+    Output("labels", "children"),
+    Input("drop-experiment", "children")
+)
+
+def display_experiment(children, *args):
+    if children is not None:
+        return button_list(items)
+    else:
+        return items
