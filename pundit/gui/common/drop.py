07f570b506f184f06065dc9d9d4e4c13f17311e7
lucien150 <55390506+lucien150@users.noreply.github.com>
2022-02-02 20:41:45 +0100

    Update drop.py

--- pundit/gui/common/drop.py
+++ pundit/gui/common/drop.py
@@ -5,12 +5,24 @@ import dash_bootstrap_components as dbc
 from dash import Input, Output, html, dcc
 from pundit.constant import PATH_EXPERIMENTS
 
+
 files = os.listdir(PATH_EXPERIMENTS)
 final_list = []
+items = []
+button_id = []
 for file in files:
     if file.endswith(".json"):
         final_list.append(file.replace(".json", ""))
 items = [dbc.DropdownMenuItem(i, id=i) for i in final_list]
+
+
+def fix_final_list():
+    files = os.listdir(PATH_EXPERIMENTS)
+    for file1 in files:
+        if file1.endswith(".json"):
+            final_list.append(file1.replace(".json", ""))
+    items = [dbc.DropdownMenuItem(i, id=i) for i in final_list]
+    return items
 
 
 dropdown1 = html.Div(
@@ -20,16 +32,20 @@ dropdown1 = html.Div(
             label="Choose Experiment"
         ),
         html.P(id="drop-experiment", className="mt-3"),
-    ]
+    ],
+    id="menu"
 )
+
 @app.callback(
     Output("drop-experiment", "children"),
-    [Input(i, "n_clicks") for i in final_list]
+    [Input(i, "n_clicks") for i in final_list],
+    prevent_initial_callbacks=True
 )
 
 
 def display_experiment(*args):
     ctx = dash.callback_context
     if ctx.triggered:
-        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
-        return "The Experiment is: " + button_id
+        fix_final_list()
+        button_id.append(ctx.triggered[0]["prop_id"].split(".")[0])
+        return "The Experiment is: " + button_id[len(button_id)-1]
