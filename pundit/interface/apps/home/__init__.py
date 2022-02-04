460972655eb2940519ef74510e1c617562e61fb0
AlexTsiamas <97591187+AlexTsiamas@users.noreply.github.com>
2022-02-04 13:41:07 +0100

    Update __init__.py

--- pundit/interface/apps/home/__init__.py
+++ pundit/interface/apps/home/__init__.py
@@ -7,6 +7,8 @@ from pundit.interface.app import app
 layout = html.Div([
     html.Div([
         html.H1('PUNDIT'),
-        html.P('Welcome to the PUNDIT app!')
+        html.H2('decision support system for website design optimization'),
+        html.P('Welcome to the PUNDIT app!'),
+        html.P('Go to Input Page to insert values or to Result Page to view the results!')
     ])
 ])
