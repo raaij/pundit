6a82bc3bcca74770f77e2f79d25d3965292a246b
lucien150 <55390506+lucien150@users.noreply.github.com>
2022-02-04 19:36:00 +0100

    Update __init__.py

--- pundit/interface/apps/result/__init__.py
+++ pundit/interface/apps/result/__init__.py
@@ -194,8 +194,8 @@ def get_asset_summary(asset_type, data):
     ]]
     dff = dff.groupby(asset_type)[['impressions', 'clicks']].sum().reset_index()
     dff[asset_type] = f'{asset_type.title()} ' + dff[asset_type].apply(str)
-    dff['ctr (%)'] = (100 * np.round(dff['clicks'] / dff['impressions'], 4)).astype(str).apply(lambda x: x[:4])
-    dff = dff.sort_values('ctr (%)', ascending=False)[:10]
+    dff['ctr'] = np.round(100.0 * (dff['clicks'] / dff['impressions']), 2)
+    dff = dff.sort_values('ctr', ascending=False)
     return dbc.Table.from_dataframe(dff, hover=True)
 
 def get_graph_summary(data):
